ae8e1b18e07264ccb267d6143fc23c167b153e30
Bogdan Kolesnikov <holmatro686@gmail.com>
2020-03-06 14:05:31 +0200

    HW#19 Add factory and transaction to model and controller

--- app/code/Bogdank/SupportChat/Model/Support.php
+++ app/code/Bogdank/SupportChat/Model/Support.php
@@ -5,7 +5,19 @@ declare(strict_types=1);
 
 namespace Bogdank\SupportChat\Model;
 
+use http\Message;
+use Magento\Framework\Exception\LocalizedException;
 
+/**
+ * @method int getMessageId()
+ * @method $this setMessageId(int $messageId)
+ * @method int getUserId()
+ * @method $this setUserId(int $userId)
+ * @method int getMessage()
+ * @method $this setMessage(string $message)
+ * @method int getWebsiteId()
+ * @method $this setWebsiteId(int $websiteId)
+ */
 class Support extends \Magento\Framework\Model\AbstractModel
 {
     /**
@@ -15,5 +27,30 @@ class Support extends \Magento\Framework\Model\AbstractModel
     {
         parent::_construct();
         $this->_init(\Bogdank\SupportChat\Model\ResourceModel\Support::class);
+    }
+
+    public function beforeSave(): self
+    {
+        parent::beforeSave();
+
+        // @TODO: see the AbstractModel::validateBeforeSave() method and its' implementation for better implementation
+        $this->validate();
+
+        return $this;
+    }
+
+
+    /**
+     * @throws LocalizedException
+     */
+    public function validate(): void
+    {
+        if (!$this->getUserId()) {
+            throw new LocalizedException(__('Can\'t send message: %s is not set.', 'user_id'));
+        }
+        if (!$this->getWebsiteId()) {
+            throw new LocalizedException(__('Can\'t send message: %s is not set.', 'website_id'));
+        }
+
     }
 }
