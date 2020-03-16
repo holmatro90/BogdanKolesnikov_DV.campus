d94604862ab589bc5e5fa8fc3c51ccc01974430e
Bogdank <holmatro686@gmail.com>
2020-03-17 01:34:09 +0200

    HW#21 Add di.xml, add code to CustomerData

--- app/code/Bogdank/SupportChat/CustomerData/CustomerMessage.php
+++ app/code/Bogdank/SupportChat/CustomerData/CustomerMessage.php
@@ -20,4 +20,44 @@ class CustomerMessage implements \Magento\Customer\CustomerData\SectionSourceInt
      * @var \Magento\Store\Model\StoreManagerInterface
      */
     private $storeManager;
+
+    /**
+     * CustomerPreferences constructor.
+     * @param \Magento\Customer\Model\Session $customerSession
+     * @param \Bogdank\SupportChat\Model\ResourceModel\SupportMessage\CollectionFactory $messageCollectionFactory
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     */
+    public function __construct(
+        \Magento\Customer\Model\Session $customerSession,
+        \Bogdank\SupportChat\Model\ResourceModel\SupportMessage\CollectionFactory $messageCollectionFactory,
+        \Magento\Store\Model\StoreManagerInterface $storeManager
+    ) {
+        $this->customerSession = $customerSession;
+        $this->messageCollectionFactory = $messageCollectionFactory;
+        $this->storeManager = $storeManager;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getSectionData(): array
+    {
+        // @TODO: we must deal with deleted attributes or deleted attribute options
+        if ($this->customerSession->isLoggedIn()) {
+            $data = [];
+            /** @var MessageCollection $messageCollectionFactory */
+            $messageCollection = $this->messageCollectionFactory->create();
+            $messageCollection->addCustomerFilter((int) $this->customerSession->getId())
+                ->addWebsiteFilter((int) $this->storeManager->getWebsite()->getId());
+
+            /** @var Preference $customerMessage */
+            foreach ($messageCollection as $customerMessage) {
+                $data[$customerMessage->getAttributeCode()] = $customerMessage->getPreferredValues();
+            }
+        } else {
+            $data = $this->customerSession->getData('customer_preferences') ?? [];
+        }
+
+        return $data;
+    }
 }
